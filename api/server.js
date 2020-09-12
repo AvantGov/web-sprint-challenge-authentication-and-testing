814fd6ec93153250937fcbd01ab48d9c27341e89
avantgov <alexcouts18@gmail.com>
2020-09-12 11:25:01 -0500

    initial branching commit with knex session store created in server file with configurations. typo modified in readme document.

--- api/server.js
+++ api/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const session = require("express-session");
+const KnexSessionStore = require("connect-session-store")(session)
+const database_access = require("../database/dbConfig")
 
 const authenticate = require('../auth/authenticate-middleware.js');
 const authRouter = require('../auth/auth-router.js');
@@ -11,6 +14,15 @@ const server = express();
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+server.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: 'set up in environment',
+    store: new KnexSessionStore({
+        knex: database_access,
+        createtable: true
+    })
+}))
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
